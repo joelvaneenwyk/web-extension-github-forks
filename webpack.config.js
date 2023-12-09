ce621c102294509c47324512580f821006731189
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2023-12-09 11:39:55 -0800

    Add GitHub action to run Webpack (#1)
    * Add GitHub action to run Webpack
    * Upgrade packages and update Webpack config
    * nit: Silenced Markdownlint noise
    * nit: Use consistent casing for 'active-forks'
    * Upgrade to latest Yarn
    * Add 'generate' to package scripts which builds and packages
    * nit: Add new words to cSpell workspace dictionary
    * Remove unsupported Node 16.x

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,25 @@ const path = require("path");
 module.exports = {
   mode: "production",
   entry: {
-    activeforks: "./dist/activeforks.js",
-    options: "./dist/options/options.js",
-    background: "./dist/background.js",
-  },
+    active_forks: "./dist/active-forks.js",
+  options: "./dist/options/options.js",
+  background: "./dist/background.js",
+},
   output: {
-    filename: "bundles/[name].bundle.js",
+  filename: "bundles/[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        loader: "style-loader!css-loader",
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        enforce: "pre",
-      },
-    ],
+module: {
+  rules: [
+    {
+      test: /\.css$/,
+      use: ["style-loader", "css-loader"]
+    },
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      // enforce: "pre",
+    },
+  ],
   },
 };
